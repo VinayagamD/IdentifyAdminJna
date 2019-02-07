fefe83da9d940e6dadd2e61a2dc19f8e9c9651e4
VinayagamD <vinayganeshdxj@gmail.com>
2019-02-08 02:25:11 +0530

    Initial commit

---
+++ src/main/java/com/vinay/isadminjna/TestAdmin.java
@@ -0,0 +1,27 @@
+package com.vinay.isadminjna;
+
+import com.sun.jna.platform.win32.Advapi32;
+import com.sun.jna.platform.win32.Advapi32Util;
+import com.sun.jna.platform.win32.WinNT;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class TestAdmin {
+    private static Supplier<WinNT.PSIDByReference> psidByReferenceSupplier =  WinNT.PSIDByReference::new;
+    private static Consumer<Advapi32Util.Account> accountConsumer = (s) -> System.out.println(s.sidString);
+   private static Predicate<Advapi32Util.Account> isAdminGroup = (s)->{
+       WinNT.PSIDByReference sid = psidByReferenceSupplier.get();
+       Advapi32.INSTANCE.ConvertStringSidToSid(s.sidString,sid);
+       return Advapi32.INSTANCE.IsWellKnownSid(sid.getValue(), WinNT.WELL_KNOWN_SID_TYPE.WinBuiltinAdministratorsSid);
+
+   };
+    public static void main(String[] args) {
+        boolean isAdmin = Arrays.asList(Advapi32Util.getCurrentUserGroups()).stream().anyMatch(isAdminGroup);
+        System.out.println(isAdmin);
+        Arrays.asList(Advapi32Util.getCurrentUserGroups()).forEach(accountConsumer);
+
+    }
+}
